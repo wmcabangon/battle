b0aaa9b0249cc641b59cb3515fc6ea99c6bee405
William <wmcabangon@gmail.com>
2017-09-15 14:42:57 +0100

    removed global variable

--- app.rb
+++ app.rb
@@ -8,21 +8,23 @@ class Battle < Sinatra::Base
   get '/' do
     erb :index
   end
+
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
+    @game = Game.instance
     erb :play
   end
 
   post '/attack' do
-    Attack.run($game.opponent_of($game.current_turn))
-    if $game.game_over?
+    @game = Game.instance
+    Attack.run(@game.opponent_of(@game.current_turn))
+    if @game.game_over?
       redirect '/game-over'
     else
       redirect '/attack'
@@ -30,17 +32,18 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @game = $game
+    @game = Game.instance
     erb :attack
   end
 
   post '/switch-turns' do
-    $game.switch_turns
+    @game = Game.instance
+    @game.switch_turns
     redirect '/play'
   end
 
   get '/game-over' do
-    @game = $game
+    @game = Game.instance
     erb :game_over
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,14 @@ class Game
 
   attr_reader :current_turn
 
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
+  end
+
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @current_turn = player_1
